89cab04a9e0cfbcada715da7b5d719b08dabf850
yassineferjani <yassineferjani9@gmail.com>
2022-12-18 00:13:28 +0100

    Add Update and add Contravention

--- GestionPermis-FE/gestionPermis/src/app/Contravention/ajouter-contravention/ajouter-contravention.component.ts
+++ GestionPermis-FE/gestionPermis/src/app/Contravention/ajouter-contravention/ajouter-contravention.component.ts
@@ -1,4 +1,10 @@
 import { Component, OnInit } from '@angular/core';
+import { FormBuilder, FormControl, FormGroup, Validators } from '@angular/forms';
+import { Router } from '@angular/router';
+import { Contravention } from 'src/app/Models/Contravention';
+import { Permis } from 'src/app/Models/Permis';
+import { ContraventionService } from 'src/app/Services/contravention.service';
+import { PermisService } from 'src/app/Services/permis.service';
 
 @Component({
   selector: 'app-ajouter-contravention',
@@ -6,10 +12,72 @@ import { Component, OnInit } from '@angular/core';
   styleUrls: ['./ajouter-contravention.component.css']
 })
 export class AjouterContraventionComponent implements OnInit {
-
-  constructor() { }
+  myform: FormGroup;
+  message!: String;
+  contravention! : Contravention;
+  idPermis!:any;
+  permis_!:Permis;
+  permis!:Permis[];
+  constructor(private servicePermis: PermisService,private service: ContraventionService, private fb: FormBuilder,private route: Router) {
+    let formControls = {
+      motif: new FormControl('', [Validators.required]),
+      retaitPoints: new FormControl('', [Validators.required]),
+      date: new FormControl('', [Validators.required]),
+      permis: new FormControl('', [Validators.required]),
+    };
+    this.myform = this.fb.group(formControls);
+   }
 
   ngOnInit(): void {
+    this.getAllPermis()
+  }
+
+  selectChangeHandler(event: any) {
+    this.idPermis = event.target.value;
+    this.getPermis(this.idPermis);
+  }
+  
+  saveContravention() {
+  
+    this.contravention=this.myform.value;
+    this.contravention.permis = this.permis_;
+
+    this.service.AddContravention(this.contravention).subscribe({
+      next:resp => {
+        this.message = "Success !";
+        this.route.navigateByUrl("/allContravention")
+
+      },
+      error:error => {
+        console.log(error);
+        this.message = error;
+      }
+  });
+  }
+
+  getAllPermis(){
+    this.servicePermis.getPermis().subscribe({
+      next:resp=>{
+        this.permis=resp;
+        return this.permis
+      },
+      error:error=>{
+        this.message= error;
+      }
+    })
+  }
+
+  getPermis(id : string){
+    this.servicePermis.get1Permis(id).subscribe({
+      next:res=>{
+        this.permis_=res;
+       
+      },
+      error:error=>{
+        this.message=error
+      }
+      
+    })
   }
 
 }
--- GestionPermis-FE/gestionPermis/src/app/Contravention/update-contravention/update-contravention.component.ts
+++ GestionPermis-FE/gestionPermis/src/app/Contravention/update-contravention/update-contravention.component.ts
@@ -1,4 +1,11 @@
+import { DatePipe } from '@angular/common';
 import { Component, OnInit } from '@angular/core';
+import { FormBuilder, FormControl, FormGroup, Validators } from '@angular/forms';
+import { ActivatedRoute, Router } from '@angular/router';
+import { Contravention } from 'src/app/Models/Contravention';
+import { Permis } from 'src/app/Models/Permis';
+import { ContraventionService } from 'src/app/Services/contravention.service';
+import { PermisService } from 'src/app/Services/permis.service';
 
 @Component({
   selector: 'app-update-contravention',
@@ -6,10 +13,78 @@ import { Component, OnInit } from '@angular/core';
   styleUrls: ['./update-contravention.component.css']
 })
 export class UpdateContraventionComponent implements OnInit {
-
-  constructor() { }
-
+  myform: FormGroup;
+  message!: String;
+  contravention! : Contravention;
+  idPermis!:any;
+  permis_!:Permis;
+  permis!:Permis[];
+  date!:any;
+  
+  constructor(private service: ContraventionService, private servicePermis:PermisService ,private fb: FormBuilder,private route: Router,
+    private datePipe: DatePipe, private activatedRoute:ActivatedRoute) { 
+      let formControls = {
+        motif: new FormControl('', [Validators.required]),
+        retaitPoints: new FormControl('', [Validators.required]),
+        date: new FormControl('', [Validators.required]),
+        permis: new FormControl('', [Validators.required]),
+      };
+      this.myform = this.fb.group(formControls);
+     }
   ngOnInit(): void {
-  }
+    let id=this.activatedRoute.snapshot.paramMap.get('id');
+    this.getAllPermis()
+    
+  this.service.get1Contravention(id).subscribe({
+    next:(res)=>{
+      this.contravention=res
+      this.date = this.datePipe.transform(this.contravention.date,'yyyy-MM-dd')
+    },
+    error:err=>{
+      console.log(err);
+    }})
+}
 
+
+ OnupdateContravention()
+ {
+  this.contravention.permis = this.permis_
+  this.contravention.date = this.myform.value.date
+  this.contravention.retaitPoints = this.myform.value.retaitPoints
+  this.contravention.motif = this.myform.value.motif
+  this.service.UpdateContravention(this.contravention).subscribe({
+    next:data=>{
+    alert("mise a jour effectuée avec succes");
+    this.route.navigateByUrl("/allContravention")
+   },
+   error:err=>{console.log(err)}})
+   
+  }
+  getPermis(id : string){
+    this.servicePermis.get1Permis(id).subscribe({
+      next:res=>{
+        this.permis_=res;
+       
+      },
+      error:error=>{
+        this.message=error
+      }
+      
+    })
+  }
+  selectChangeHandler(event: any) {
+    this.idPermis = event.target.value;
+    this.getPermis(this.idPermis);
+  }
+  getAllPermis(){
+    this.servicePermis.getPermis().subscribe({
+      next:resp=>{
+        this.permis=resp;
+        return this.permis
+      },
+      error:error=>{
+        this.message= error;
+      }
+    })
+  }
 }

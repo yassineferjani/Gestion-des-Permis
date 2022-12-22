00e9f549d3f8202b3bbbbc5a3905a0abaa5e03eb
yassineferjani <yassineferjani9@gmail.com>
2022-12-22 19:16:06 +0100

    Added Reactive programming

--- GestionPermis-FE/gestionPermis/src/app/Services/conducteur.service.ts
+++ GestionPermis-FE/gestionPermis/src/app/Services/conducteur.service.ts
@@ -1,7 +1,8 @@
 import { HttpClient, HttpHeaders } from '@angular/common/http';
 import { ErrorHandler, Injectable } from '@angular/core';
-import { catchError, Observable, throwError } from 'rxjs';
+import { catchError, Observable, throwError,map } from 'rxjs';
 import { Conducteur } from '../Models/Conducteur';
+import { Response } from '../Models/Response';
 
 @Injectable({
   providedIn: 'root'
@@ -10,13 +11,23 @@ export class ConducteurService {
 
   constructor(private http: HttpClient) {}
 
-    private host = 'http://127.0.0.1:8080/conducteur';httpOptions = {
+    private host = 'http://127.0.0.1:8080/conducteur';
+    httpOptions = {
       headers: new HttpHeaders({
         'Content-Type': 'application/json'
       })} 
     private errorMessage!:string;
     
-
+      conducteurs$=<Observable<Response>>this.http.get<Array<Conducteur>>(this.host + '/all').pipe(
+        map(response=>{
+          return{status:2 ,data:{conducteurs: response}}as Response;
+        }),
+        catchError(err=>{
+          this.errorMessage=err.message;
+          return throwError(err);
+        })
+      );
+      
     getConducteurs(): Observable<Array<Conducteur>> {
       return this.http.get<Array<Conducteur>>(this.host + '/all').pipe(
         catchError(err=>{
